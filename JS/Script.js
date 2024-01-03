52ec1eb87d32d077ddc0fcabf3e727cf053e1d9b
Diego <diegogomes4@hotmail.com>
2024-01-03 17:54:45 -0300

    2024

---
+++ JS/Script.js
@@ -0,0 +1,10 @@
+let elemento = null
+
+elemento = document.documentElement
+console.log(elemento)
+console.log(typeof(elemento))
+console.log(elemento.lang)
+
+elemento = document.head
+console.log(elemento)
+console.table(elemento)
